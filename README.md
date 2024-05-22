f6035b31c9398dc48a83f9b674c444ade5407677
mauriciodev <mauricio.dev@gmail.com>
2024-05-22 18:02:11 -0300

    Some improvements

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # batch_ppp
+
+## Download rtklib explorer
+git clone https://github.com/rtklibexplorer/RTKLIB.git  
+cd RTKLIB/app/consapp/rnx2rtkp/gcc  
+make  
+cd ../../../../..  
+cp RTKLIB/app/consapp/rnx2rtkp/gcc/rnx2rtkp ./  
+
+## Configure the markdown
+config.yml  
+
+## Configure the templates
+templates/rtklib_template_brdc.conf  
+
+## Run
+python ppp_batch.py -c config.yml  
--- ppp_batch.py
+++ ppp_batch.py
@@ -15,6 +15,7 @@ from tqdm import tqdm
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import yaml
 
 
 #This method is not really used, but it can be used to clean GNSS data.
@@ -56,26 +57,26 @@ def downloadIonex(doy,year):
         get_ipython().system('gunzip $local_filename -f')
     return local_filename
 
-def run_rt_ppp(ppp_executable, obsFile, outFile, template_conf, replaceDict):
+def run_rt_ppp(ppp_executable, obsFile, outFile, template_conf, replaceDict, cwd = '.'):
     #rtkcmd=f'./rnx2rtkp -x 2 -y 0 -k temporary.conf -o {outFile} {obsFile} {navFile} {navFile2}'
     if not os.path.exists(outFile):
         temporaryConf(replaceDict, template_conf, 'temporary.inp')
         cmd = f"{ppp_executable} {obsFile} temporary.inp"
         #obsFile = rinex_with_gps_only(obsFile) #cleaning to leave only GPS data
         print(f"Running {cmd}")
-        result=subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        result=subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, cwd=cwd)
         os.rename('output/RT_PPP.out', outFile)
     df = pd.read_csv(outFile, comment='%', sep='\s+', parse_dates=True, header='infer')
     pos = df[['X(m)', 'Y(m)', 'Z(m)']].mean(axis=0).to_numpy()
     return pos
 
-def run_rtklib(ppp_executable, obsFile, navFile, template_conf, replaceDict):
+def run_rtklib(ppp_executable, obsFile, navFile, template_conf, replaceDict, cwd = '.'):
     out_file = obsFile.split('.')[0]+'.rtklib'#'temp.obs'
     if not os.path.exists(out_file):
         temporaryConf(replaceDict, template_conf, 'temporary.inp')
         cmd=f'{ppp_executable} -x 2 -y 0 -k temporary.inp -o {out_file} {obsFile} {navFile}'
         print(f"Running {cmd}")
-        result=subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        result=subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, cwd=cwd)
     if not os.path.exists(out_file):
         return np.array([np.nan, np.nan, np.nan])
     header = ['GPST', 'x-ecef(m)', 'y-ecef(m)', 'z-ecef(m)', 'Q', 'ns', 'sdx(m)', 'sdy(m)', 'sdz(m)', 'sdxy(m)', 'sdyz(m)', 'sdzx(m)', 'age(s)', 'ratio']
@@ -86,29 +87,22 @@ def run_rtklib(ppp_executable, obsFile, navFile, template_conf, replaceDict):
 def absError(m):
     return np.sqrt(np.sum(np.array(m)**2,axis=1))
 
-def main():
 
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('config',  type=argparse.FileType('r'), default='config.yml')
-
+def main(config):
     #CONFIGURATION
-    ppp_solution = 'rtklib' # either 'rt_ppp' or 'rtklib'
-    if ppp_solution =='rt_ppp':
-        ppp_executable = "./rt_ppp" #In windows: ..\rt_ppp.exe
-        run_folder = './rt_ppp'
-        template_conf = 'templates/rt_ppp_template_brdc.conf'
+    if config['ppp_solution'] =='rt_ppp':
         run_ppp_method = run_rt_ppp #this is a function
-    if ppp_solution == 'rtklib':
-        ppp_executable = "rnx2rtkp" #In windows: ..\rt_ppp.exe
-        run_folder = './'
-        template_conf = 'templates/rtklib_template_brdc.conf'
+    elif config['ppp_solution'] == 'rtklib':
         run_ppp_method = run_rtklib #this is a function
-    save_array_as = 'out.npy'
-    end_year = 2015
-    start_year = 2015
-    station = "onrj"
-    basePos = [4283638.3610, -4026028.8230, -2466096.8370]
+    ppp_executable = config['ppp_executable'] #In windows: ..\rt_ppp.exe
+    run_folder = config['run_folder']
+    template_conf = config['ppp_template_conf']
+        
+
+    save_array_as = os.path.join(run_folder, config['experiment_name'])
+    end_year = config['end_year']
+    start_year = config['start_year']
+    station = config['station']
     
     d0=date(year=start_year, month=1, day=1)
     d1=date(year=end_year, month=12, day=31)
@@ -132,9 +126,8 @@ def main():
         else:
             print(f"skipping {zipFile}")
 
-
-    os.chdir(run_folder)
-    print(os.getcwd())
+    if not os.path.exists(run_folder): os.makedirs(run_folder, exist_ok=True)
+    print(f"Running in {run_folder}")
 
     test_run = subprocess.run(ppp_executable, shell=True)
     if test_run.returncode==127:
@@ -146,7 +139,6 @@ def main():
     error=[]
 
     #basecommand="./rnx2rtkp -x 0 -y 0 -k "
-    basecommand=ppp_executable
 
     for day in tqdm(pd.date_range(d0,d1,freq='D')):
         os.makedirs('output',exist_ok=True)
@@ -171,13 +163,13 @@ def main():
 
         replaceDict = {
             '{ionex}': ionex,
-            '{x0}' : basePos[0],
-            '{y0}' : basePos[1],
-            '{z0}' : basePos[2],
+            '{x0}' : config["reference_position"][0],
+            '{y0}' : config["reference_position"][1],
+            '{z0}' : config["reference_position"][2],
             }
 
         position = run_ppp_method(ppp_executable, obsFile, navFile, template_conf, replaceDict = replaceDict)
-        error.append(position - basePos)
+        error.append(position - config["reference_position"])
 
     error = np.array(error)
     np.save(save_array_as,error)
@@ -185,39 +177,14 @@ def main():
 
 
 
-def plot():
-    df = pd.DataFrame()
-    df['date'] = pd.date_range(d0,d1,freq='D')
-    df['CODG_ex'] = codgError[:,0]
-    df['CODG_ey'] = codgError[:,1]
-    df['CODG_ez'] = codgError[:,2]
-    df['CODG_norm'] = np.linalg.norm(codgError,axis=1)
-    df['Klobuchar_ex'] = brdcError[:,0]
-    df['Klobuchar_ey'] = brdcError[:,1]
-    df['Klobuchar_ez'] = brdcError[:,2]
-    df['Klobuchar_norm'] = np.linalg.norm(brdcError,axis=1)
-    csvFileName = f'output/data_{d0}_to_{d1}.csv'
-    print(f"Saving the errors to {csvFileName}")
-    df.to_csv(csvFileName)
-    df.plot(x='date', y=['CODG_ex', 'CODG_ey', 'CODG_ez'], figsize=(10, 10))
-    df.plot(x='date', y=['Klobuchar_ex', 'Klobuchar_ey', 'Klobuchar_ez'], figsize=(10, 10))
-    df.plot(x='date', y=['CODG_norm', 'Klobuchar_norm'], figsize=(10, 10))
 
-
-
-
-    pos_mean = [df['X(m)'].mean(), df['Y(m)'].mean(), df['Z(m)'].mean()]
-
-    df['err_X'] = df['X(m)']-df['X(m)'].mean()
-    df['err_Y'] = df['Y(m)']-df['Y(m)'].mean()
-    df['err_Z'] = df['Z(m)']-df['Z(m)'].mean()
-
-    df['err_norm'] = np.linalg.norm(df[['err_X', 'err_Y', 'err_Z']],axis=1)
-
-
-    df.plot(x='GPS_TIME', y = ['err_X', 'err_Y', 'err_Z', 'err_norm'])
-
-    df.plot(x='GPS_TIME', y = 'err_norm')
 
 if __name__ == "__main__":
-    main()
+    
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-c' '-config',  type=argparse.FileType('r'), default='config.yml')
+    parsed_args = parser.parse_args()
+    config = yaml.safe_load(parsed_args.c_config)
+    
+    main(config)
