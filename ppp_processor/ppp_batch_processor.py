d8d17308e6efd900cf0becd7c1095aa19a3ed4f5
Luiz Andrade <lcoandrade@gmail.com>
2024-05-28 18:45:09 -0500

    Update folders

--- ppp_processor/ppp_batch_processor.py
+++ ppp_processor/ppp_batch_processor.py
@@ -31,7 +31,7 @@ class PPPBatchProcessor():
         subprocess.run(f'Binary/teqc -E -C -R -S -O.obs L1L2C1P2S1S2 +out {newFile} {rinexFile}', shell=True)
         return newFile
     
-    def temporaryConf(self, replaceDict:dict, templateFile:str="template.conf", temporaryFile:str="temporary.conf"):
+    def temporaryConf(self, replaceDict:dict, templateFile:str="template.conf", temporaryFile:str="temporary.conf") -> None:
         with open(templateFile, 'r') as template:
             template_text=template.read()
             for key, value in replaceDict.items():
@@ -88,27 +88,39 @@ class PPPBatchProcessor():
 
     def main(self):
         # Getting configs from the yaml file
+        experiment_name = self.config['experiment_name']
+        run_folder = self.config['run_folder']
         if self.config['ppp_solution'] =='rt_ppp':
             run_ppp_method = self.run_rt_ppp #this is a function
-        elif config['ppp_solution'] == 'rtklib':
+        elif self.config['ppp_solution'] == 'rtklib':
             run_ppp_method = self.run_rtklib #this is a function
-        ppp_executable = config['ppp_executable']
-        run_folder = config['run_folder']
-        template_conf = config['ppp_template_conf']
-        save_array_as = os.path.join(run_folder, config['experiment_name'])
-        end_year = config['end_year']
-        start_year = config['start_year']
-        station = config['station']
+        ppp_executable = self.config['ppp_executable']
+        template_conf = self.config['ppp_template_conf']
+        start_year = self.config['start_year']
+        end_year = self.config['end_year']
+        station = self.config['station']
+        reference_position = self.config['reference_position']
+        save_array_as = self.config['experiment_name']
+        ionex_pattern = self.config['ionex_pattern']
         
+        # Defining dates for Jan, 1st and Dec, 31st
         d0=date(year=start_year, month=1, day=1)
         d1=date(year=end_year, month=12, day=31)
 
-        print(f"Downloading data from IBGE for station {station}")
+        # Station folder
+        station_folder = os.path.join(run_folder, station)
+
+        # Experiment folder
+        experiment_folder = os.path.join(run_folder, experiment_name)
+
+        print(f"Running in {run_folder}...")
+        print(f"Downloading data from IBGE for station {station}...")
         for day in tqdm(pd.date_range(d0,d1,freq='D')):
             link=f'https://geoftp.ibge.gov.br/informacoes_sobre_posicionamento_geodesico/rbmc/dados/{day.year}/{day.day_of_year:03}/{station}{day.day_of_year:03}1.zip'
             rbmcfile=link.split("/")[-1]
-            zipFile=os.path.join(str(day.year),rbmcfile)
-            os.makedirs(str(day.year),exist_ok=True)
+            year_folder = os.path.join(station_folder, str(day.year))
+            zipFile=os.path.join(year_folder, rbmcfile)
+            os.makedirs(year_folder, exist_ok=True)
             if not os.path.exists(zipFile) or os.path.getsize(zipFile)<1024:
                 try:
                     urllib.request.urlcleanup()
@@ -120,14 +132,12 @@ class PPPBatchProcessor():
                     if os.path.exists(zipFile):
                         os.unlink(zipFile)
             else:
-                print(f"skipping {zipFile}")
+                print(f"skipping {zipFile}")        
 
-        if not os.path.exists(run_folder): os.makedirs(run_folder, exist_ok=True)
-        print(f"Running in {run_folder}")
-
+        # Checking if the executable is available
         test_run = subprocess.run(ppp_executable, shell=True)
         if test_run.returncode==127:
-            print(f"Could not find PPP executable in {ppp_executable}.")
+            print(f"Could not find a PPP executable in {ppp_executable}.")
             sys.exit(0)
         else:
             print(f"Using PPP executable: {ppp_executable}.")
@@ -136,39 +146,46 @@ class PPPBatchProcessor():
 
         #basecommand="./rnx2rtkp -x 0 -y 0 -k "
 
+        # Output folder
+        output_folder = os.path.join(experiment_folder, 'output')
+
+        # Current folder
+        current_folder = os.path.join(experiment_folder, 'current')
+
         for day in tqdm(pd.date_range(d0,d1,freq='D')):
-            os.makedirs('output',exist_ok=True)
+            os.makedirs(output_folder, exist_ok=True)
             #day=pd.to_datetime(d0)+pd.Timedelta('117D')
             year=str(day.year)
+            year_folder = os.path.join(station_folder, year)
             fname=f'{station}{day.day_of_year:03}1.zip'
-            if not os.path.exists(os.path.join(year,fname)):
+            if not os.path.exists(os.path.join(year_folder, fname)):
                 error.append([0,0,0])
                 print(day,day.day_of_year)
                 continue
             #downloadIonex(day.day_of_year,day.year)
 
             y2d=day.year%100
-            outFilePos=os.path.join('output',f'{year}_{fname.replace(".zip",".pos")}')
-            obsFile=os.path.join('current',fname.replace(".zip",f".{y2d}o"))
-            navFile=os.path.join('current',fname.replace(".zip",f".{y2d}n"))
-            navFile2=os.path.join('current',fname.replace(".zip",f".{y2d}g"))
+            outFilePos=os.path.join(output_folder, f'{year}_{fname.replace(".zip",".pos")}')
+            obsFile=os.path.join(current_folder, fname.replace(".zip",f".{y2d}o"))
+            navFile=os.path.join(current_folder, fname.replace(".zip",f".{y2d}n"))
+            navFile2=os.path.join(current_folder, fname.replace(".zip",f".{y2d}g"))
             ionex=f"ionex/codg{day.day_of_year:03}0.{day.year%100}i"
 
-
-            shutil.unpack_archive(os.path.join(year,fname), 'current')
+            # Unzipping the station file to the current folder
+            shutil.unpack_archive(os.path.join(year_folder, fname), current_folder)
 
             replaceDict = {
                 '{ionex}': ionex,
-                '{x0}' : config["reference_position"][0],
-                '{y0}' : config["reference_position"][1],
-                '{z0}' : config["reference_position"][2],
+                '{x0}' : reference_position[0],
+                '{y0}' : reference_position[1],
+                '{z0}' : reference_position[2],
                 }
 
             position = run_ppp_method(ppp_executable, obsFile, navFile, template_conf, replaceDict = replaceDict)
-            error.append(position - config["reference_position"])
+            error.append(position - reference_position)
 
         error = np.array(error)
-        np.save(save_array_as,error)
+        np.save(save_array_as, error)
 
 if __name__ == "__main__":
     
