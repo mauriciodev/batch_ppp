625bd028f0be08816f045ffd30867acdad866319
Luiz Andrade <lcoandrade@gmail.com>
2024-06-08 14:08:02 -0300

    dealing with URL errors

--- ppp_preprocessor/preprocessor.py
+++ ppp_preprocessor/preprocessor.py
@@ -5,6 +5,8 @@ import yaml
 import pandas as pd
 from tqdm import tqdm
 import subprocess
+import time
+import random
 
 class Preprocessor():
     def __init__(self, config) -> None:
@@ -19,6 +21,24 @@ class Preprocessor():
         d1=pd.to_datetime(end_date) #date(year=end_year, month=12, day=31)
 
         return d0, d1
+    
+    def download_from_url(self, url:str, file:str):
+        if not os.path.exists(file) or os.path.getsize(file) == 0:
+            print(f'Downloading {url} to {file}...')
+            try:
+                request.urlcleanup()
+                request.urlretrieve(url, file)
+                if os.path.getsize(file) == 0:
+                    print(f'File {file} has size 0. Erasing.')
+                    os.unlink(file)
+                else:
+                    print(f'File saved to {file}.')
+            except error.URLError as e:
+                print(f"Failed to download {file} from {url}")
+                print(f"Error: {e.reason}/Error code: {e.errno}")
+                if os.path.exists(file): os.unlink(file)
+        else:
+            print(f'Skipping {file}.')
 
     def download_station(self) -> None:
         run_folder = self.config['run_folder']
@@ -37,18 +57,7 @@ class Preprocessor():
             year_folder = os.path.join(station_folder, str(day.year))
             zipFile=os.path.join(year_folder, rbmcfile)
             os.makedirs(year_folder, exist_ok=True)
-            if not os.path.exists(zipFile) or os.path.getsize(zipFile)<1024:
-                try:
-                    request.urlcleanup()
-                    local_filename, headers = request.urlretrieve(link, zipFile)
-                    if os.path.getsize(zipFile)<1024: os.unlink(zipFile)
-                except error.URLError as e:
-                    print(f"Failed {zipFile} from {link}")
-                    print(f"Error: {e.reason}")
-                    if os.path.exists(zipFile):
-                        os.unlink(zipFile)
-            else:
-                print(f"skipping {zipFile}")        
+            self.download_from_url(link, zipFile)       
 
     def download_ionex(self, prefix='codg') -> None:
         ionex_folder = self.config['ionex_folder']
@@ -61,14 +70,11 @@ class Preprocessor():
             local_filename = os.path.join(ionex_folder, baseurl.split('/')[-1])
             dcb_file = local_filename.replace('.Z','')
             if not os.path.exists(dcb_file):
-                print(f'Downloading {baseurl} to {local_filename}')
-                request.urlretrieve(baseurl, local_filename)
-                print(f'File saved to {local_filename}')
-                if os.path.getsize(local_filename) == 0:
+                self.download_from_url(baseurl, local_filename)
+                if not os.path.exists(local_filename):
                     print('Trying to get rapid, because final ionex was not found.')
                     baseurl=baseurl.replace(prefix,"corg")
-                    request.urlretrieve(baseurl, local_filename)
-
+                    self.download_from_url(baseurl, local_filename)
                 subprocess.run(f'gunzip {local_filename} -f', shell=True)
     
 if __name__ == '__main__':
@@ -79,6 +85,7 @@ if __name__ == '__main__':
     config = yaml.safe_load(parsed_args.c_config)
 
     preprocessor = Preprocessor(config)
+
     preprocessor.download_station()
     preprocessor.download_ionex(prefix='codg')
     preprocessor.download_ionex(prefix='c1pg')
